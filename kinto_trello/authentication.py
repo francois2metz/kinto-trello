a4cea23b539f695c11ae92a6378751586a051c8e
François de Metz <francois@2metz.fr>
2017-11-06 00:47:05 +0100

    Fix the syntax.

--- kinto_trello/authentication.py
+++ kinto_trello/authentication.py
@@ -12,7 +12,6 @@ TRELLO_METHOD = 'trello'
 def trello_apikey(request):
     return request.registry.settings['trello.apikey']
 
-
 @implementer(IAuthenticationPolicy)
 class TrelloAuthenticationPolicy(CallbackAuthenticationPolicy):
     def __init__(self, realm='Realm'):
@@ -36,19 +35,19 @@ class TrelloAuthenticationPolicy(CallbackAuthenticationPolicy):
             return None
 
         if not hasattr(request.registry, 'cache'):
-            return fetch_trello(request, token)
+            return self.fetch_trello(request, token)
 
         cache = request.registry.cache
         cache_key = "token_trello:" + token
         user_id = cache.get(cache_key)
 
         if not user_id:
-            user_id = fetch_trello(request, token)
+            user_id = self.fetch_trello(request, token)
             cache.set(cache_key, user_id, ttl=3600*24)
 
         return user_id
 
-    def fetch_trello(self, request, token)
+    def fetch_trello(self, request, token):
         try:
             params = {"token": token, "key": trello_apikey(request)}
             resp = requests.get("https://api.trello.com/1/members/me", params=params)
